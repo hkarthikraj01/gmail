189fe1453a6873033195f9878c45db5622becf76
hkarthikraj01 <53332719+hkarthikraj01@users.noreply.github.com>
2020-11-30 15:39:40 +0530

    Add files via upload

---
+++ scripts/auth.js
@@ -0,0 +1,87 @@
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+  if (user) {
+    db.collection('guides').onSnapshot(snapshot => {
+      setupGuides(snapshot.docs);
+      setupUI(user);
+    }, err => console.log(err.message));
+  } else {
+    setupUI();
+    setupGuides([]);
+  }
+});
+
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  db.collection('guides').add({
+    title: createForm.title.value,
+    content: createForm.content.value
+  }).then(() => {
+    // close the create modal & reset form
+    const modal = document.querySelector('#modal-create');
+    M.Modal.getInstance(modal).close();
+    createForm.reset();
+  }).catch(err => {
+    console.log(err.message);
+  });
+});
+
+// signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  // sign up the user & add firestore data
+  auth.createUserWithEmailAndPassword(email, password)
+  .then(() => {
+      console.log('Signed Up Successfully !');
+      sendVerificationEmail();
+  })
+  .catch(error => {
+      console.error(error);
+  })
+});
+const sendVerificationEmail = () => {
+  //Built in firebase function responsible for sending the verification email
+  auth.currentUser.sendEmailVerification()
+  .then(() => {
+      console.log('Verification Email Sent Successfully !');
+      //redirecting the user to the profile page once everything is done correctly
+      window.location.assign('../profile');
+  })
+  .catch(error => {
+      console.error(error);
+  })
+}
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut();
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+
+});
